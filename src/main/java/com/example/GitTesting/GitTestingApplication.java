ea8776ae5530b167dcda6eb2eaf3af0887106b6c
RaisAhmad <raisahmadd7@gmail.com>
2021-10-07 10:46:44 +0500

    initial comit

--- src/main/java/com/example/GitTesting/GitTestingApplication.java
+++ src/main/java/com/example/GitTesting/GitTestingApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitTestingApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitTestingApplication.class, args);
 	}
